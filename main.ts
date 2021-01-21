6648fbaa51b0017c23365e1899f816c06b5739be
Radu <radu.nicolae.botezatu@gmail.com>
2021-01-21 20:00:42 +0100

    implementation done

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 namespace ShowOff {
-    
+
+    export function fillAtPos(pos: number): void {
+        pos |= 0;
+        const x = Math.floor(pos % 5);
+        const y = Math.floor(pos / 5);
+        led.plot(x, y);
+    }
 }
