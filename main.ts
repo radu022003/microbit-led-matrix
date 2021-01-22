236a33c41afe99588872250620a555181849ada6
Radu <radu.nicolae.botezatu@gmail.com>
2021-01-22 14:02:45 +0100

    added new event handler for analog read pin

--- main.ts
+++ main.ts
@@ -12,4 +12,30 @@ namespace ShowOff {
         const y = Math.floor(pos / 5);
         led.plot(x, y);
     }
+
+    let onLimitReachedHandler: (limit: number) => void;
+    let onLimitNotReachedHandler: (limit: number) => void;;
+
+    /**
+     * Registers code to run when the radio receives a number.
+     */
+    //% help=ShowOff/on-received-number
+    //% blockId=ShowOff_on_number_drag block="on radio received" blockGap=16
+    //% draggableParameters=reporter
+    export function onLimitReached(cb: (limit: number) => void) {
+        onLimitReachedHandler = cb;
+    }
+
+    export function onLimitNotReached(cb: (limit: number) => void) {
+        onLimitNotReachedHandler = cb;
+    }
+
+    export function checkLimit(pin: AnalogPin) {
+        if (pins.analogReadPin(pin) < 40){
+            onLimitReachedHandler(4);
+        }
+        else {
+            onLimitNotReachedHandler(0);
+        }
+    }
 }
--- test.ts
+++ test.ts
@@ -3,3 +3,27 @@ for (let i = 0; i < 25; i++){
     ShowOff.fillAtPos(i);
     basic.pause(100);
 }
+basic.clearScreen()
+
+ShowOff.onLimitReached(function (limit: number) {
+    basic.plotLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+})
+
+ShowOff.onLimitNotReached(function (limit: number) {
+    basic.plotLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . #
+    `)
+})
+while(true) {
+    ShowOff.checkLimit(AnalogPin.P0);
+}
